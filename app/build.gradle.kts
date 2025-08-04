1fa9d7b56bc7671c7ce19d40e4b00bad506fd2d3
Advitiya Sharda <advitiyasharda@gmail.com>
2025-08-04 02:35:01 -0700

    App with correct login and partial register and added logout and modified the HistoryActivity.kt Functioning , with UI of history a bit improved works the best with all the functions working FInal verion with all the things in place Also Added streaks features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.androidx.foundation)
+    implementation(libs.androidx.foundation.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
